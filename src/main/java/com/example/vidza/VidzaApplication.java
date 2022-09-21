5a82f7083ffceb9ab863b3dfa454f0d44847b586
sonykarl <karlmaxwatembo@gmail.com>
2022-09-21 13:22:55 +0300

    initial commit

---
+++ src/main/java/com/example/vidza/VidzaApplication.java
@@ -0,0 +1,13 @@
+package com.example.vidza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidzaApplication.class, args);
+	}
+
+}

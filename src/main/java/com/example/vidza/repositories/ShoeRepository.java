d5e2c69b537cb0608410e80bacae4554c0f618e3
sonykarl <karlmaxwatembo@gmail.com>
2022-09-28 13:12:43 +0300

    ini repo

---
+++ src/main/java/com/example/vidza/repositories/ShoeRepository.java
@@ -0,0 +1,11 @@
+package com.example.vidza.repositories;
+
+import com.example.vidza.entities.Shoe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigInteger;
+
+@Repository
+public interface ShoeRepository extends JpaRepository<BigInteger, Shoe> {
+}
